4a80b4bc0a3d8ed78286550010176bad11a3b718
Raphael Vallat <raphaelvallat9@gmail.com>
2020-04-07 11:08:36 -0700

    Updated docs

--- yasa/sleepstats.py
+++ yasa/sleepstats.py
@@ -73,10 +73,12 @@ def transition_matrix(hypno):
         >>> _, probs = transition_matrix(a)
         >>> # Start the plot
         >>> grid_kws = {"height_ratios": (.9, .05), "hspace": .1}
-        >>> f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws, figsize=(5, 5))
-        >>> sns.heatmap(probs, ax=ax, square=False, vmin=0, vmax=1, cbar=True, cbar_ax=cbar_ax,
-        ...             cmap='YlOrRd', annot=True, fmt='.2f',
-        ...             cbar_kws={"orientation": "horizontal", 'fraction': 0.1, 'label': 'Transition probability'})
+        >>> f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws,
+        ...                                 figsize=(5, 5))
+        >>> sns.heatmap(probs, ax=ax, square=False, vmin=0, vmax=1, cbar=True,
+        ...             cbar_ax=cbar_ax, cmap='YlOrRd', annot=True, fmt='.2f',
+        ...             cbar_kws={"orientation": "horizontal", "fraction": 0.1,
+        ...                       "label": "Transition probability"})
         >>> ax.set_xlabel("To sleep stage")
         >>> ax.xaxis.tick_top()
         >>> ax.set_ylabel("From sleep stage")
