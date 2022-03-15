71c0a8245c61b328a82ab1197c34b673333120a3
Raphael Vallat <raphaelvallat9@gmail.com>
2022-03-15 09:59:45 -0700

    Switch to 0.6.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ MAINTAINER_EMAIL = 'raphaelvallat9@gmail.com'
 URL = 'https://github.com/raphaelvallat/yasa/'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/raphaelvallat/yasa/'
-VERSION = '0.6.0'
+VERSION = '0.6.1'
 PACKAGE_DATA = {'yasa.data.icons': ['*.svg']}
 
 INSTALL_REQUIRES = [
