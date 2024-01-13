fd1e829d304cb1194f3b54b05a6c1a5cb2bb0205
Raphael Vallat <raphaelvallat9@gmail.com>
2024-01-13 14:34:21 +0100

    remove outdated in init

--- yasa/__init__.py
+++ yasa/__init__.py
@@ -9,13 +9,9 @@ from .plotting import *
 from .sleepstats import *
 from .spectral import *
 from .staging import *
-from outdated import warn_if_outdated
 
 # Define YASA logger
 logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", datefmt="%d-%b-%y %H:%M:%S")
 
 __author__ = "Raphael Vallat <raphaelvallat9@gmail.com>"
 __version__ = "0.6.4"
-
-# Warn if a newer version of YASA is available
-warn_if_outdated("yasa", __version__)
