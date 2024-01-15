c2577b6956b5231efa94cb5959a222f1a86239f7
S.J. Guillot <58807831+sjg2203@users.noreply.github.com>
2024-01-15 16:31:08 +0100

    Fix

--- yasa/tests/test_detection.py
+++ yasa/tests/test_detection.py
@@ -1,16 +1,4 @@
 """Test the functions in yasa/spectral.py."""
-#  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#   Copyright (c) 2024. Simon J. Guillot. All rights reserved.                            +
-#   Redistribution and use in source and binary forms, with or without modification, are strictly prohibited.
-#                                                                                         +
-#   THIS CODE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
-#   BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-#   IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-#   OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-#   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS CODE,
-#   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import mne
 import pytest
